a39788deab81b1008b9cf8fef7f869a125f9b3ca
n.ninovic <Eunovatio4you>
2015-08-12 17:43:00 +0200

    remove prettyfaces. Set java 6 and tomcat 7

--- src/main/java/co/prime/springboot/web/HomeBean.java
+++ src/main/java/co/prime/springboot/web/HomeBean.java
@@ -1,15 +1,10 @@
 package co.prime.springboot.web;
 
 import co.prime.springboot.service.HomeService;
-import com.ocpsoft.pretty.faces.annotation.URLAction;
-import com.ocpsoft.pretty.faces.annotation.URLActions;
-import com.ocpsoft.pretty.faces.annotation.URLMapping;
-import com.ocpsoft.pretty.faces.annotation.URLMappings;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.inject.Named;
 
 /**
  * User: n.ninovic
@@ -18,10 +13,6 @@ import javax.inject.Named;
  */
 @ManagedBean
 @SessionScoped
-@Named("homeBean")
-@URLMappings(mappings = {
-        @URLMapping(id = "home", pattern = "/", viewId = "/index.jsf")
-})
 public class HomeBean {
 
     @Autowired
@@ -30,16 +21,9 @@ public class HomeBean {
     private int randomNumber;
     private String locale = "en";//change it to sr to get text in second supported language
 
-    @URLActions(actions = {
-            @URLAction(mappingId = "home", onPostback = false)
-    })
-    public String home() {
-        randomNumber = homeService.getRandomNumber();
-        return null;
-    }
 
     public int getRandomNumber() {
-        return randomNumber;
+        return randomNumber = homeService.getRandomNumber();
     }
 
     public void setRandomNumber(int randomNumber) {
